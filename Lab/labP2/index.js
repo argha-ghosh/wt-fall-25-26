080e1894b57b8c98436dc668abbf3f623d4547e2
ghosh-argha <23-50804-1@student.aiub.edu>
2025-11-08 13:07:22 +0600

    2nd day

--- Lab/labP2/index.js
+++ Lab/labP2/index.js
@@ -1,9 +1,43 @@
-//console.log(`Hello`);
-//console.log(`I love Pizza!`)
+console.log(`Hello`);
+console.log(`I love Pizza!`)
 
 //window.alert(`Do you want to continue?`);
 
 document.getElementById("M1").textContent = `Hello World`;
 document.getElementById("MyP").textContent = `I love to eat Pizza!`;
+//document.getElementById("M2").textContent = `Hello bitches!!`;
+
+console.log(`Welcome again!!`);
+
+let x = 120;
+console.log(x); // Here 120 is printed
+
+let age = 25;
+let price = 100;
+let CGPA = 3.2;
+let Name = "Borsha";
+
+console.log(typeof price); //"type off shows what kind of data is this."
+console.log(`Her name is ${Name}.`);
+console.log(`I am ${age} years old boy.`);
+console.log(`This is $${price} cents`);
+console.log(`My CGPA is ${CGPA}.`);
+
+let FullName = "Borsha";
+let ageb = 24;
+
+document.getElementById("B1").textContent = `Her name is ${FullName}.`;
+document.getElementById("B2").textContent = `She is ${ageb} years old`;
+
+let students = 30;
+
+//students = students + 1;
+//students = students - 1;
+//students = students * 2;
+//students = students / 2;
+
+console.log(students);
+
+
 
 
