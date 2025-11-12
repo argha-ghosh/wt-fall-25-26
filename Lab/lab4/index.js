5458f7117ee485c3f37bb7835c91e1d1f0da31f9
ghosh-argha <23-50804-1@student.aiub.edu>
2025-11-12 17:04:54 +0600

    Updated

---
+++ Lab/lab4/index.js
@@ -0,0 +1,10 @@
+console.log("Hello World");
+
+var a = 5;
+console.log(a);
+var s = "AIUBIAN";
+console.log(s.length);
+console.log(`The length of my taken string is ${s.length}......!`);
+
+document.getElementById("lab1").textContent = `I love to eat Pizza!`;
+
